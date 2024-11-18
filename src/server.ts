9990fdfe703218404f584128a3b475759584c5b3
Md. shofiqul islam <shofiqebr@gmail.com>
2024-11-19 02:23:23 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+  
+}
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
