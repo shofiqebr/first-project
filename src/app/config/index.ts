5f775342d964431959f2dcde07f993a762b569ae
Md. shofiqul islam <shofiqebr@gmail.com>
2024-11-28 07:31:09 +0600

    add eslint

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -5,4 +5,7 @@ import path from 'path'
 
  dotenv.config({path: path.join((process.cwd(), '.env'))})
 
- 
+ export default {
+    port: process.env.Port,
+    database_url: process.env.DATABASE_URL
+ }
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,24 @@
 
 // getting-started.js
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
+import config from "./app/config";
+import app from "./app";
 
-main().catch(err => console.log(err));
+
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+ try {
+
+  await mongoose.connect(config.database_url as string);
 
   
+  app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+ }catch(error){
+  console.log(error)
+ }
+
 }
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+main()
